a4d21f86c05c1ffd490055f6d167d5fd8cd0f92a
Sudhakar R <sudhakar.ar96@gmail.com>
2026-07-19 18:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Python DevOps Application deployed successfully!"
+    return "Welcome to My DevOps Project!"
 
 @app.route("/health")
 def health():
